7e702a4b2dcbb06c1cd74cc280ed8beb6ae0a713
mohammad-yahia <m.y.alqudah@gmail.com>
2021-06-05 13:37:31 +0300

    updatedatabase

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -77,13 +77,24 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mohammadalqudah$Aljomleh-database',
+        'HOST': 'mohammadalqudah.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+        'USER': 'mohammadalqudah',
+        'PASSWORD': 'Yahia@123',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
